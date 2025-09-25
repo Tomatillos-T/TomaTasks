debe8266e70a758f91662f0f32b6c21ac251596f
Adrian-TJ <atrevino136@gmail.com>
2025-09-25 11:35:22 -0600

    Testing for Cors and table creation

---
+++ MtdrSpring/backend/src/main/java/com/springboot/TomaTask/config/CorsConfig.java
@@ -0,0 +1,39 @@
+package com.springboot.TomaTask.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+import java.util.Collections;
+import java.util.List;
+/*
+    This class configures CORS, and specifies which methods are allowed
+    along with which origins and headers
+    @author: peter.song@oracle.com
+
+ */
+@Configuration
+public class CorsConfig {
+    Logger logger = LoggerFactory.getLogger(CorsConfig.class);
+    
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","OPTIONS","DELETE","PATCH"));
+        config.addAllowedHeader("*");
+        config.addExposedHeader("location");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        CorsFilter filter = new CorsFilter(source);
+        return filter;
+    }
+
+}
