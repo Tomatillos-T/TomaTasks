1d9f75e31ae2236c1fe4677630cd30f5fbe2c492
Adrian-TJ <atrevino136@gmail.com>
2025-09-29 09:00:58 -0600

    User api changes

--- MtdrSpring/backend/src/main/java/com/springboot/TomaTask/controller/UserController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/TomaTask/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     @Autowired
     private UserService UserService;
     //@CrossOrigin
-    @GetMapping(value = "/user")
+    @GetMapping(value = "/api/user")
     public List<User> getAllUsers(){
         return UserService.findAll();
     }
